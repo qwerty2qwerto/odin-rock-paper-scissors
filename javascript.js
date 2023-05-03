ee19a19e44f5f3e1961f762fe3a52eaba4a2f094
Lucas <123847994+qwerty2qwerto@users.noreply.github.com>
2023-05-03 14:55:43 -0400

    Code playRound function in JS, code game function in JS

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-//Pseudocode
-//
-//Create function getComputerChoice() {
-//  Generate random number 
-//
 
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function getPlayerChoice() {
     playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
@@ -14,6 +11,8 @@ function getPlayerChoice() {
         playerSelection !== 'scissors') {
             alert(`Try again dumbass.`);
             location.reload();
+    } else {
+        return playerSelection;
     }
 }
 
@@ -21,11 +20,40 @@ function getComputerChoice() {
     let numericComputerSelection = Math.floor((Math.random()) * 3) + 1;
     if (numericComputerSelection === 1) {
         computerSelection = 'rock';
+        return computerSelection;
     } else if (numericComputerSelection === 2) {
         computerSelection = 'paper';
+        return computerSelection;
     } else {
         computerSelection = 'scissors';
+        return computerSelection;
     }
 }
 
-getPlayerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        alert('Tie! You both chose ' + playerSelection + ".");
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+                (playerSelection === 'paper' && computerSelection === 'rock') ||
+                (playerSelection === 'scissors' && computerSelection === 'paper')) {
+                    alert('You won! ' + playerSelection + " beats " + computerSelection);
+                    ++playerScore;
+    } else {
+        alert('You lost! ' + computerSelection + " beats " + playerSelection);
+        ++computerScore;
+    }
+}
+
+function game() {
+    getPlayerChoice();
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+game();
+game();
+game();
+game();
+game();
+
+alert('You won ' + playerScore + ' rounds. The computer won ' + computerScore + ' rounds.');
